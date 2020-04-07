dec169fdc44c99f6715da7e6f39773996e15301e
Parth Patel <parth.patel@cognizant.com>
2020-04-07 16:07:46 -0500

    updated test class and CD jenkinsfile

--- functionalTest/src/test/java/FunctionalTest.java
+++ functionalTest/src/test/java/FunctionalTest.java
@@ -7,10 +7,32 @@ import io.restassured.specification.RequestSpecification;
 
 class FuntionalTest {
 
+
+
 	@Test
 	void getUserDetailsTest() {
 		//The base URI to be used
-		RestAssured.baseURI = "http://10.106.210.84:9090/book";
+		String port = System.getProperty("server.port");
+		if (port == null) {
+			RestAssured.port = Integer.valueOf(9090);
+		}
+		else{
+			RestAssured.port = Integer.valueOf(port);
+		}
+
+		String basePath = System.getProperty("server.base");
+		if(basePath==null){
+			basePath = "/book/";
+		}
+		RestAssured.basePath = basePath;
+
+		String baseHost = System.getProperty("server.host");
+		if(baseHost==null){
+			baseHost = "http://10.106.210.84";
+		}
+		RestAssured.baseURI = baseHost;
+
+		//RestAssured.baseURI = "http://10.106.210.84:9090/book";
 
 		//Define the specification of request. Server is specified by baseURI above.
 		RequestSpecification httpRequest = RestAssured.given();
