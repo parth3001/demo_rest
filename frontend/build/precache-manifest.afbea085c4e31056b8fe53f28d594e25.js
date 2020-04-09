88e98b210e61e632e85c3746d3b9e4048fd858dd
Parth Patel <parth.patel@cognizant.com>
2020-04-09 09:56:55 -0500

    added employee UI code

---
+++ frontend/build/precache-manifest.afbea085c4e31056b8fe53f28d594e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a39698d1656e6112e1f184cf3ad3d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662857adec3dcde06441",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ad20c37fb7a73bb6e9c",
+    "url": "/static/js/2.723f5fff.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.723f5fff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "662857adec3dcde06441",
+    "url": "/static/js/main.0b517a94.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
